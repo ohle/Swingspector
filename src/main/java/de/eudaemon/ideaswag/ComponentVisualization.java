4cce56a6dee60dfce6f76d47ab5b0b0fab0fea08
Ohle Claussen <claussen@jpk.com>
2022-01-09 17:39:23 +0100

    Add a set or rulers

--- src/main/java/de/eudaemon/ideaswag/ComponentVisualization.java
+++ src/main/java/de/eudaemon/ideaswag/ComponentVisualization.java
@@ -1,17 +1,25 @@
 package de.eudaemon.ideaswag;
 
 import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.Stroke;
+
+import java.awt.geom.AffineTransform;
 
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 import javax.swing.border.EmptyBorder;
 
+import com.intellij.openapi.editor.colors.EditorColors;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.ui.Gray;
 import com.intellij.util.ui.JBInsets;
 
@@ -20,6 +28,7 @@ import de.eudaemon.swag.SizeInfos;
 class ComponentVisualization extends JPanel {
 
     private static final int SIZE_CUTOFF = 200;
+    private static final int RULER_SIZE = 20;
 
     private final SizeInfos sizing;
     private final BufferedImage snapshot;
@@ -35,6 +44,7 @@ class ComponentVisualization extends JPanel {
     private final RunningComponent component;
 
     ComponentVisualization(RunningComponent component_) {
+        System.out.println(UIManager.getColor(EditorColors.GUTTER_BACKGROUND));
         component = component_;
         sizing = component.getSizeInfos();
         snapshot = component.getSnapshot().getImage();
@@ -44,10 +54,16 @@ class ComponentVisualization extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        drawCheckerBoard(g2d);
+        paintRulers(g2d);
+        g2d.translate(RULER_SIZE, RULER_SIZE);
+        paintCheckerBoard(g2d);
         if (snapshot != null) {
             g2d.drawImage(snapshot, 0, 0, null);
         }
+        paintSizeRectangles(g2d);
+    }
+
+    private void paintSizeRectangles(Graphics2D g2d) {
         g2d.setColor(ComponentInfoPanel.MAX_SIZE_COLOR);
         if (isMaximumSizeCropped()) {
             g2d.setStroke(croppedStroke);
@@ -64,7 +80,54 @@ class ComponentVisualization extends JPanel {
         g2d.drawRect(0, 0, sizing.minimumSize.size.width, sizing.minimumSize.size.height);
     }
 
-    private void drawCheckerBoard(Graphics2D g) {
+    private void paintRulers(Graphics2D g) {
+        EditorColorsScheme editorScheme = EditorColorsManager.getInstance().getGlobalScheme();
+        Color bg = editorScheme.getColor(EditorColors.GUTTER_BACKGROUND);
+        Color fg = editorScheme.getColor(EditorColors.LINE_NUMBERS_COLOR);
+        RenderingHints oldRenderingHints = g.getRenderingHints();
+
+        g.setColor(bg);
+        g.fillRect(0, 0, getWidth(), RULER_SIZE);
+        g.fillRect(0, 0, RULER_SIZE, getHeight());
+        g.setColor(fg);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+        for (int x = RULER_SIZE; x < getWidth(); x += 2) {
+            int len = ticLength(x - RULER_SIZE);
+            g.drawLine(x, RULER_SIZE, x, RULER_SIZE - len);
+        }
+        for (int y = RULER_SIZE; y < getHeight(); y += 2) {
+            int len = ticLength(y - RULER_SIZE);
+            g.drawLine(RULER_SIZE - len, y, RULER_SIZE, y);
+        }
+
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        for (int x = 100; x < getWidth(); x += 100) {
+            String label = String.valueOf(x);
+            double width = g.getFontMetrics().getStringBounds(label, g).getWidth();
+            g.drawString(label, (int) (x + RULER_SIZE - width / 2.0), RULER_SIZE - 10);
+        }
+        for (int y = 100; y < getWidth(); y += 100) {
+            AffineTransform transform = g.getTransform();
+            String label = String.valueOf(y);
+            double width = g.getFontMetrics().getStringBounds(label, g).getWidth();
+            g.rotate(3.0 * Math.PI / 2.0, RULER_SIZE - 10, y + RULER_SIZE);
+            g.drawString(label, (int) (RULER_SIZE - 10 - width / 2.0), y + RULER_SIZE);
+            g.setTransform(transform);
+        }
+        g.setRenderingHints(oldRenderingHints);
+    }
+
+    private int ticLength(int x) {
+        int len = 5;
+        if (x % 50 == 0) {
+            len = 10;
+        } else if (x % 10 == 0) {
+            len = 7;
+        }
+        return len;
+    }
+
+    private void paintCheckerBoard(Graphics2D g) {
         int squareSize = 5;
         for (int y = 0; y * squareSize < getHeight(); y++) {
             for (int x = 0; x * squareSize < getWidth(); x++) {
