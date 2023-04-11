4ee06259c79f32fd276e138cf03855149f39eb22
Ohle Claussen <claussen@jpk.com>
2023-04-11 20:09:14 +0200

    Disable refresh when stopped in debugger

--- src/main/java/de/eudaemon/ideaswag/RefreshAction.java
+++ src/main/java/de/eudaemon/ideaswag/RefreshAction.java
@@ -1,12 +1,26 @@
 package de.eudaemon.ideaswag;
 
+import com.intellij.debugger.DebuggerManagerEx;
+import com.intellij.debugger.engine.DebugProcessImpl;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
 
 import org.jetbrains.annotations.NotNull;
 
 public class RefreshAction extends AnAction {
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        if (project != null) {
+            DebugProcessImpl debugProcess =
+                    DebuggerManagerEx.getInstanceEx(project).getContext().getDebugProcess();
+            e.getPresentation()
+                    .setEnabled(debugProcess == null || debugProcess.getSession().isRunning());
+        }
+    }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
